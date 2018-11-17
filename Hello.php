1a54de36ca862b92eee01d8bbc87adf40c2223cf
BoLan <brian7lan3@gmail.com>
2018-11-17 19:44:56 +0800

    if-else邏輯判斷

--- Hello.php
+++ Hello.php
@@ -1,45 +1,36 @@
 <?php
 
-$myArray = array("巧克力", "牛奶", "冰淇淋");
-// echo $myArray;
-print_r($myArray);
-echo "<hr>";
-print_r($myArray[1]);
-echo "<hr>";
-echo $myArray[2];
-echo "<hr>";
 
-$anotherArray[1] = "草莓";
-$anotherArray[0] = "果醬";
-$anotherArray[5] = "吐司";
-$anotherArray[3] = "測試";
-print_r($anotherArray);
-echo "<hr>";
+$a = true;
+$b = true;
 
-$thirdArray = array(
-    "eng" => "英文",
-    "kor" => "韓文",
-    "jap" => "日文"
-);
-print_r($thirdArray);
-echo "<hr>";
+if ($a && $b == true) {
+    echo "正確!";
+} else {
+    echo "錯誤!";
+}
 
-echo $thirdArray["eng"];
-echo "<hr>";
 
-$anotherArray[] = "沙拉";
-print_r($anotherArray);
-echo "<hr>";
+if ($a and $b == true) {
+    echo "2.正確!";
+} else {
+    echo "錯誤!";
+}
 
-unset($thirdArray["eng"]);
-print_r($thirdArray);
-echo "<hr>";
+if ($a AND $b == true) {
+    echo "3.正確!";
+} else {
+    echo "錯誤!";
+}
 
-$name = "ken";
-echo $name;
 
-unset($name);
-echo $name;
+
+
+
+
+
+
+
 
 
 
