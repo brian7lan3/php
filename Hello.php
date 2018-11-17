b42fc045a6560b3c2fdda851e12f7595b7ce5beb
BoLan <brian7lan3@gmail.com>
2018-11-17 19:59:50 +0800

    foreach應用:位置與值

--- Hello.php
+++ Hello.php
@@ -1,7 +1,14 @@
 <?php
 
-    for($i=0; $i<10; $i++){
-        echo $i."<hr>";
+    $myBolan = array("貓","狗","牛");
+
+    foreach ($myBolan as $key => $value){
+        echo "位置： $key , 值：$value <hr>";
     }
+
+
+// for ($i = 0; $i < 10; $i = $i + 2) {
+//     echo $i . "<hr>";
+// }
 
 ?>
