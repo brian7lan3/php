a9dcd7b6263accd1470628bbb633be5bdf034039
BoLan <brian7lan3@gmail.com>
2018-11-17 19:49:19 +0800

    for迴圈 裡面不能用var

--- Hello.php
+++ Hello.php
@@ -1,37 +1,7 @@
 <?php
 
-
-$a = true;
-$b = true;
-
-if ($a && $b == true) {
-    echo "正確!";
-} else {
-    echo "錯誤!";
-}
-
-
-if ($a and $b == true) {
-    echo "2.正確!";
-} else {
-    echo "錯誤!";
-}
-
-if ($a AND $b == true) {
-    echo "3.正確!";
-} else {
-    echo "錯誤!";
-}
-
-
-
-
-
-
-
-
-
-
-
+    for($i=0; $i<10; $i++){
+        echo $i."<hr>";
+    }
 
 ?>
