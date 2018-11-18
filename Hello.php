aa9e4f57208219df07e6cff4e1cf8712647119af
BoLan <brian7lan3@gmail.com>
2018-11-18 17:04:42 +0800

    用while顯示數組的位置跟值

--- Hello.php
+++ Hello.php
@@ -2,17 +2,17 @@
 
     $myBolan = array("貓","狗","牛");
 
-    // $i = 0;
-
-    // while($myBolan[$i]){
-    //     echo "位置: $i , 值: $myBolan[$i] <br/> ";
-    //     $i++;
-    // }
-
     $i = 0;
-    while($i <= 10){
-        echo $i."<br/>";
+
+    while($myBolan[$i]){
+        echo "位置: $i , 值: $myBolan[$i] <br/> ";
         $i++;
     }
+
+    // $i = 0;
+    // while($i <= 10){
+    //     echo $i."<br/>";
+    //     $i++;
+    // }
 
 ?>
