99bdad2c1c1bde38782b13deb1b579bcd6b6844c
BoLan <brian7lan3@gmail.com>
2018-11-18 17:30:42 +0800

    郵件發送失敗!

--- Hello.php
+++ Hello.php
@@ -1,18 +1,16 @@
 <?php
 
-    $myBolan = array("貓","狗","牛");
-
-    $i = 0;
-
-    while($myBolan[$i]){
-        echo "位置: $i , 值: $myBolan[$i] <br/> ";
-        $i++;
+    $emailTo = "a3673802@yahoo.com.tw";
+    $title = "Test Email";
+    $body = "This is email content.";
+    $headers = "From:brian7lan3@gmail.com";
+    
+    
+    if(mail($emailTo, $title, $body, $headers)){
+        echo "郵件發送成功";
+    }else{
+        echo "郵件發送失敗!";
     }
 
-    // $i = 0;
-    // while($i <= 10){
-    //     echo $i."<br/>";
-    //     $i++;
-    // }
 
 ?>
